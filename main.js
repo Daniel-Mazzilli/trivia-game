9196bad2cc8208e410488607007a15efb53192e8
Daniel-Mazzilli <danmazzilli@pursuit.org>
2022-10-03 22:05:47 -0400

    changing up the app

--- main.js
+++ main.js
@@ -1,67 +1,68 @@
-const baseURL = `https://opentdb.com/api.php?amount=10&type=multiple`;
+// Fetch url
+const baseURL = `https://opentdb.com/api.php?amount=10`;
+let category;
+const multiChoice = `&type=multiple`;
 
 // Getters
 const question = document.querySelector(`#question`);
 const answers = document.querySelector(`#answers`);
-const next = document.querySelector(`#next`);
-const scoreSpan = document.querySelector(`.score`);
-// console.log(question, answers, next)
+const select = document.querySelector(`select`);
+const form = document.querySelector(`form`);
 
 // Variables
 let updatedResults;
 let index = 0;
 let score = 0;
 
+// Game functionality
+form.addEventListener(`submit`, (event) => {
+  event.preventDefault();
+  getQuestions();
+});
+
 const updateQuestion = () => {
   question.innerHTML = updatedResults[index].question;
-
   for (let i = 0; i < 4; i++) {
     const div = document.createElement(`div`);
     const randomNum = Math.floor(
       Math.random() * updatedResults[index].incorrect_answers.length
     );
-
     div.innerHTML = updatedResults[index].incorrect_answers.splice(
       randomNum,
       1
     );
-
     answers.append(div);
   }
 };
 
 const getQuestions = async () => {
-  //   fetch(baseURL)
-  //     .then((res) => res.json())
-  //     .then((res) => {
-  //       console.log(res);
-  //     })
-  //     .catch((err) => console.log(EvalError));
-  const qs = await fetch(baseURL);
+  if (select.value) {
+    category = `&category=` + select.value;
+  } else {
+    category = ``;
+  }
+  const qs = await fetch(baseURL + category + multiChoice);
   const qsJson = await qs.json();
-  //   console.log(qsJson);
 
   updatedResults = qsJson.results.map((q) => {
     q.incorrect_answers.push(q.correct_answer);
     return q;
   });
-
-  //   console.log(updatedResults);
   updateQuestion();
 };
 
-next.addEventListener(`click`, () => {
-  index++;
+// next.addEventListener(`click`, () => {
+//   index++;
 
-  if (index >= updatedResults.length) {
-    question.innerHTML = `GAME OVER`;
-    answers.innerHTML = ``;
-  } else {
-    answers.innerHTML = ``;
-    updateQuestion();
-  }
-  answers.style.pointerEvents = `auto`;
-});
+//   if (index >= updatedResults.length) {
+//     question.innerHTML = `GAME OVER`;
+//     answers.innerHTML = ``;
+//   } else {
+//     answers.innerHTML = ``;
+//     updateQuestion();
+//   }
+//   answers.style.pointerEvents = `auto`;
+// });
 
 answers.addEventListener(`click`, (event) => {
   console.log(event.target.innerHTML);
@@ -70,11 +71,8 @@ answers.addEventListener(`click`, (event) => {
     score += 10;
     scoreSpan.innerHTML = score;
     event.target.style.backgroundColor = `lightgreen`;
-    event.target.style.border = `2px solid green`;
   } else {
     event.target.style.backgroundColor = `red`;
-    event.target.style.border = `2px solid maroon`;
   }
   answers.style.pointerEvents = `none`;
 });
-getQuestions();
